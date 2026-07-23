4c8b72399f3c2c6fc264433b1087e20fd45f4e25
sasquach67 <elephon08@gmail.com>
2026-07-23 18:49:01 -0400

    feat(ui): add completion and parked primitives

---
+++ src/components/atlas/index.ts
@@ -0,0 +1,11 @@
+export {
+  AtlasAgentAvatar,
+  AtlasBranch,
+  AtlasBubble,
+  AtlasInput,
+  AtlasMarker,
+  AtlasMessage,
+  AtlasMessageScroller,
+  AtlasTweetCard,
+  AtlasTypewriterText,
+} from './parked-primitives'
